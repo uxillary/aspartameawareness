fa31be44f21cd9f261527b12183aa86da2c27cb1
adamski <adamjch@proton.me>
2025-09-14 16:14:57 +0100

    Add MDX and sitemap integrations to Astro config
    Updated astro.config.mjs to include @astrojs/mdx and @astrojs/sitemap integrations, set the site URL, and reordered integration imports. Also updated dependencies in package.json and tsconfig.json to support new features and latest versions.

--- aa-astro/astro.config.mjs
+++ aa-astro/astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  site: "https://aspartameawareness.org",
+  integrations: [react(), tailwind(), mdx(), sitemap()],
 });
